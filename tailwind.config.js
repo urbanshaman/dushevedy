5bb533387a22421cc60f7b1db7d3c500a5d3a4ca
urbanshaman <urbanshaman@ya.ru>
2022-03-12 16:21:53 +0300

    upd svg

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,12 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {},
+      fontFamily: {
+        'cursive': ['"Bad Script"', 'cursive']
+	    }
+	  },
   },
   variants: {
     extend: {},
